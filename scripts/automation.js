0613bd386a0fb7fc0576858e3dc3f403d9088d64
ju445m <ju445m@att.com>
2021-11-02 12:24:36 -0600

    schedules done

--- scripts/automation.js
+++ scripts/automation.js
@@ -1,49 +1,12 @@
+const driver = require('../helpers/chrome.helper').getChromeDriver();
 const { until, By } = require("selenium-webdriver");
+const { addConsoleHandler } = require('selenium-webdriver/lib/logging');
 
-var driver = require("./chrome.helper").getChromeDriver();
 
-const findElementBy = async (locator) => {
-  const webElement = driver.wait(until.elementLocated(locator), 20000);
-  return driver.wait(until.elementIsVisible(webElement), 20000);
-}
-
-const getSchedules = async () => {
+const run = async () => {
   await driver.get('http://www.nfl.com/schedules/');
   var elems = await driver.findElements(By.className('nfl-o-matchup-group'));
-  let data = await processElements(elems);
-  return data;
-}
-
-const processElements = async (elems) => {
-  let elements = await Promise.all(elems);
-  let dataResult = await Promise.all(elements.map(async el => {
-    let data = {
-      played: [],
-      scheduled: []
-    };
-    let text = await el.getText();
-    let dates = text.split("  ");
-    let result = processSchedule(dates);
-    data.played = result.played;
-    data.scheduled = result.scheduled;
-    return data;
-  }));
-  let finalResult = {
-    played: [],
-    scheduled: []
-  };
-  for (let i = 0; i < dataResult.length; i++) {
-    let res = dataResult[i];
-    for (let j = 0; j < res.played.length; j++) {
-      let play = res.played[j];
-      finalResult.played.push(play);
-    }
-    for (let j = 0; j < res.scheduled.length; j++) {
-      let schedule = res.scheduled[j];
-      finalResult.scheduled.push(schedule);
-    }
-  }
-  return finalResult;
+  return elems;
 }
 
 const processFinal = (final, date) => {
@@ -51,6 +14,8 @@ const processFinal = (final, date) => {
   for (let i = 0; i < final.length; i++) {
     if (i === 0) {
       game.date = date;
+    } else if (i === 1) {
+      game.hour = final[i];
     } else if (i === 2) {
       game.scoreOne = final[i];
     } else if (i === 3) {
@@ -69,18 +34,24 @@ const processNotPlayed = (toPlay, date) => {
   for (let i = 0; i < toPlay.length; i++) {
     if (i === 0) {
       if (toPlay[i].includes(":")) {
-        game.date = `${date} ${toPlay[i]} ${toPlay[i + 1]}`;
-      } else {
         game.date = `${date} ${toPlay[i + 1]} ${toPlay[i + 2]}`;
+      } else {
+        game.date = `${date} ${toPlay[i]} ${toPlay[i + 1]}`;
       }
     }
     else if (i === 2) {
+      if (toPlay[0].includes(":")) {
+        game.channel = toPlay[i];
+      } else {
+        game.channel = toPlay[i + 1];
+      }
+    } else if (i === 3) {
       if (toPlay[0].includes(":")) {
         game.teamOne = toPlay[i];
       } else {
         game.teamOne = toPlay[i + 1];
       }
-    } else if (i === 3) {
+    } else if (i === 4) {
       if (toPlay[0].includes(":")) {
         game.teamTwo = toPlay[i];
       } else {
@@ -110,10 +81,29 @@ const processSchedule = (array) => {
     played: played,
     scheduled: scheduled
   };
+  console.log("check", data);
   return data;
 }
+const result = run();
 
-module.exports = {
-  findElementBy: findElementBy,
-  getSchedules: getSchedules
-}
+result.then(success => {
+  let data = {
+    played: [],
+    scheduled: []
+  }
+  Promise.all(success).then(elements => {
+    elements.forEach(el => {
+      el.getText().then(text => {
+        let dates = text.split("  ");
+        let result = processSchedule(dates);
+        data.played.concat(result.played);
+        data.scheduled.concat(result.scheduled);
+        console.log(data);
+      }).catch(error => {
+        console.log(error);
+      });
+    })
+  })
+}).catch(error => {
+  console.log(error);
+})
